6a4f116d85180309d0b6f92ba11e044ffbf70589
Mark Boyd <mark.boyd@gsa.gov>
2022-05-04 14:50:48 -0400

    rename test

--- broker_test.go
+++ broker_test.go
@@ -159,7 +159,7 @@ func TestGetDatabaseName(t *testing.T) {
 	}
 }
 
-func TestCreateBrokerDatabase(t *testing.T) {
+func TestCreateBrokerDatabaseSuccess(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
